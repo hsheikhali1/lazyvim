ecd8c7d6629f86da162e7f3cdfb60b932c48de57
Harun Sheikhali <sufisheikhali@gmail.com>
2023-07-02 12:33:01 -0400

    fixed my config

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,29 @@
+return {
+  "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
+  opts = {
+    ---@type lspconfig.options
+    servers = {
+      tsserver = {
+        settings = {
+          typescript = {
+            updateImportsOnFileMove = {
+              enabled = "prompt",
+            },
+            autoClosingTags = true,
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+        },
+      },
+    },
+  },
+}
